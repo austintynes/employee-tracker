be9f7b13f21433658d54cd835dd96d54279ab48c
austintynes <austintynes10@gmail.com>
2023-10-25 14:56:25 -0500

    structured employeeTracker.js

---
+++ employeeTracker.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const Department = require("./Department");
+const Role = require("./Role");
+const Employee = require("./Employee");
+
+class EmployeeTracker {
+    async start() {
+
+    }
+}
+
+module.exports = EmployeeTracker;
