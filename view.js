0d7320f2467cb026bac258de540605e458f8d1c2
austintynes <austintynes10@gmail.com>
2023-10-27 17:15:58 -0500

    Added funtionality in node

--- view.js
+++ view.js
@@ -2,10 +2,12 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "your_username",
-  password: "your_password",
-  database: "your_database_name",
+  user: "root",
+  password: "Password",
+  database: "ccd_db",
 });
+
+
 
 function begin() {
   inquirer
@@ -13,12 +15,47 @@ function begin() {
       {
         type: "list",
         message: "What would you like to do?",
-        choices: ["View all departments", "View all roles"],
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update employee role"],
         name: "begin",
       },
     ])
 
     .then((answer) => {
-      console.log(answer);
+      if (answer.begin === 'View all departments') {
+        connection.query("select * FROM department", (err, res) => {
+          console.table(res);
+          begin();
+        })
+      } else if (answer.begin === 'View all roles') {
+        connection.query("select * FROM role", (err, res) => {
+          console.table(res);
+          begin();
+        })
+      } else if (answer.begin === 'View all employee') {
+        connection.query("select * FROM employee", (err, res) => {
+          console.table(res);
+          begin();
+        })
+        
+      } else if (answer.begin === 'Add a department') {
+          inquirer.prompt([{
+            type: 'input',
+            message: 'What is the name of the department?: ',
+            name: "addDept"
+          }
+           
+          ])
+
+          .then((deptAnswer) => {
+            connection.query("INSERT INTO department (name) VALUES (?) ", [deptAnswer.addDept], (err, res) => {
+              console.table(res);
+              begin();
+            })
+          })
+      }
+     
     });
+
 }
+
+begin();
